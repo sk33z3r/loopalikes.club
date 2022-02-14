af1491f07b647ef2e030849939435ee4a3f10e45
sk33z3r <sk33z3r@protonmail.com>
2022-02-14 13:17:47 -0500

    fix: page titles

--- origins.php
+++ origins.php
@@ -11,7 +11,7 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <title>Loophead Lore</title>
-        <meta name="description" content="A fan-made site showing loopalikes the community has noticed between the Loopheads and various celebrities/characters from pop culture.">
+        <meta name="description" content="A fan-made site with origin stories for the Loopheads characters.">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="css/main.css">
     </head>
