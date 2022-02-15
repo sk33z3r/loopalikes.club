c5ac3942027deeb365d28da493115770950d5718
sk33z3r <sk33z3r@protonmail.com>
2022-02-14 20:32:23 -0500

    add: favicon

--- looplegangers.php
+++ looplegangers.php
@@ -14,6 +14,12 @@
         <meta name="description" content="A fan-made site showing loopalikes the community has noticed between the Loopheads and various celebrities/characters from pop culture.">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="css/main.css">
+
+        <!-- Favicons -->
+        <link rel="apple-touch-icon" sizes="180x180" href="images/favicon/apple-touch-icon.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="images/favicon/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="images/favicon/favicon-16x16.png">
+        <link rel="manifest" href="images/favicon/site.webmanifest">
     </head>
     <body>
         <!--[if lt IE 7]>
