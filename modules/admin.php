ff34d362b38b9ed65cb9c46c102d45508be5396c
sk33z3r <sk33z3r@protonmail.com>
2022-02-18 19:49:15 -0500

    chore: initial admin page

---
+++ modules/admin.php
@@ -0,0 +1,34 @@
+<?php
+    if ($type === "head") { ?>
+    <title>Loopalikes Admin</title>
+    <meta name="description" content="A page for doing things.">
+<?php }
+    if ($type === "body") {
+        $password = $_POST["password"];
+        include ".env.php";
+        include "modules/nav.php";
+        echo "<h1>Loopalikes Admin</h1>";
+        if ($password == "") { ?>
+            <div id="container">
+                <div id="login">
+                    <form method="post" action="index.php?page=admin">
+                        <b>PASSWORD: </b> <input type="password" name="password" /> <button type="submit">LOG IN</button>
+                    </form>
+                </div>
+            </div>
+        <?php }
+        else if ($password !== $ADMIN_PASS) { ?>
+            <div id="container">
+                <div id="login">
+                    <span class="fail">ACCESS DENIED!</span>
+                </div>
+            </div>
+        <?php }
+        else if ($password === $ADMIN_PASS) { ?>
+            <div id="container">
+                <div id="admin">
+                    <b>SUCCESS!</b>
+                </div>
+            </div>
+        <?php }
+    } ?>
