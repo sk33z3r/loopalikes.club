4bff0189247e110e6c0b09515d60e0a658842153
sk33z3r <sk33z3r@protonmail.com>
2022-02-19 13:15:25 -0500

    chore: localhost testing matches server

--- web.sh
+++ web.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 
-docker rm -f loopalikes.club
-docker run -d --rm -p 8080:80 --name loopalikes.club -v "$PWD":/var/www/html:ro php:7.2-apache
+case $1 in
+    down) docker-compose down;;
+    up) docker-compose up -d;;
+    *) echo "Invalid command: $0 [up|down]"; exit 1;;
+esac
