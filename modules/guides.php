4c2f3bf47d54d876de876a2bb6083a519183c1e3
sk33z3r <sk33z3r@protonmail.com>
2022-02-20 17:00:20 -0500

    feat: read guides from json, better UI

--- modules/guides.php
+++ modules/guides.php
@@ -3,8 +3,8 @@
     <meta name="description" content="A collection of links and guides from the Loophead community.">
 <?php }
     if ($type === "body") {
-        $json = file_get_contents("json/lore.json");
-        $lore = json_decode($json, true);
+        $json = file_get_contents("json/guides.json");
+        $guides = json_decode($json, true);
         include "modules/nav.php";
 ?>
 
@@ -14,13 +14,10 @@
         <p><small>A collection of links and guides from the Loophead community.</small></p>
         <hr noshade>
     </div>
-    <div class="text">
-        <ul>
-            <li><a href="https://reddit.com/r/loopringorg/comments/snvjhs/what_are_loopheads_a_comprehensive_guide/" target="_blank">What Are Loopheads? A Comprehensive Guide</a></li>
-            <li><a href="https://reddit.com/r/loopringorg/comments/so4tci/guide_to_move_your_loophead_from_l2_wallet_to/" target="_blank">List Your Loophead on OpenSea</a></li>
-            <li><a href="https://reddit.com/r/loopringorg/comments/siy25a/loopheads_drop_1_results_analysis_event_summary/" target="_blank">Airdrop 1 Analysis & Summary Part 1</a></li>
-            <li><a href="https://reddit.com/r/loopringorg/comments/sj1wor/loopheads_drop_1_results_analysis_event_summary/" target="_blank">Airdrop 1 Analysis & Summary Part 2</a></li>
-        </ul>
+    <div class="filters home">
+        <?php $count = 0; while ($count < count($guides) ) { ?>
+            <a href="<?php echo $guides[$count]["url"] ?>" target="_blank"><button><?php echo $guides[$count]["title"] ?></button></a>
+        <?php $count++; } ?>
     </div>
 </div>
 
