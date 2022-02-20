5af98fa7326061a411a2166dbe1c8a7f0a6cc5c5
sk33z3r <sk33z3r@protonmail.com>
2022-02-20 18:50:05 -0500

    remove: unnecessary links from nav bar

--- modules/nav.php
+++ modules/nav.php
@@ -8,8 +8,6 @@
     </div>
     <div id="ext-nav">
         <a target="_blank" href="https://loopheadexplorer.io"><button>EXPLORER</button></a>
-        <a target="_blank" href="https://loopheads.info"><button>LH.INFO</button></a>
-        <a target="_blank" href="https://loopheads.world"><button>LH.WORLD</button></a>
         <a target="_blank" href="https://opensea.io/collection/moody-brain-loopring"><button>OPENSEA</button></a>
     </div>
 </div>
