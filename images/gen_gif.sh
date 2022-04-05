d5d35f48505847824c846457abaad1fc1492aa9a
sk33z3r <sk33z3r@protonmail.com>
2022-04-05 07:58:33 -0400

    fix: sets and subsets

--- images/gen_gif.sh
+++ images/gen_gif.sh
@@ -29,6 +29,7 @@ ipfs_hashes[2000]="QmWXQ6rGWPxMtuHUdHCgsWw1tT7vkhvbzxJPc3kmH3BXKR"
 
 get_all() {
     set=1100
+    subset=100
     while [ $set -lt 2100 ]; do
         if [ $set -eq 1000 ]; then
             num_map=("_index" 6 7 8 9 10 101 102 103 104 105 106 107 108 109 11 110 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39 40 41 42 43 44 45 46 47 48 49 50 51 52 53 54 55 56 57 58 59 60 61 62 63 64 65 66 67 68 69 70 71 72 73 74 75 76 77 78 79 80 81 82 83 84 85 86 87 88 89 90 91 92 93 94 95)
@@ -43,7 +44,7 @@ get_all() {
             url_num=$(printf "%02d" ${num_map[$num]})
             mkdir -p $id/{original,vars}
             echo "      Downloading PNGs from IPFS..."
-            curl="curl -s --connect-timeout 60 --max-time 300 ${ipfs_root}${ipfs_hashes[$set]}/loophead${url_num}_${set}"
+            curl="curl -s --connect-timeout 60 --max-time 300 ${ipfs_root}${ipfs_hashes[$set]}/loophead${url_num}_${subset}"
             orig="$id/original/"
             ${curl}_0_0.png > ${orig}0-0.png &
             ${curl}_0_1.png > ${orig}0-1.png &
@@ -103,6 +104,7 @@ get_all() {
             ((num++))
         done
         set=$((set+100))
+        subset=$((subset+100))
     done
 }
 
