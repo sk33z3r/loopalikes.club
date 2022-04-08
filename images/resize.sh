bc7ebc3f2ec37d8b9a4bd53d642ccc66e545bc3f
sk33z3r <sk33z3r@protonmail.com>
2022-04-08 04:05:28 -0400

    feat: resize from list

--- images/resize.sh
+++ images/resize.sh
@@ -3,8 +3,25 @@
 s=$1
 e=$2
 
-while [ $s -lt $e ]; do
-    echo "Resizing #$s..."
-    find $s/vars -type f -name "*.png" -exec convert "{}" -resize 250x250 "{}" \;
-    ((s++))
-done
+range() {
+    if [ -z $e ]; then
+        echo "Missing ending ID"
+        exit 1
+    fi
+    while [ $s -lt $e ]; do
+        echo "Resizing #$s..."
+        find $s/vars -type f -name "*.png" -exec convert "{}" -resize 250x250 "{}" \;
+        ((s++))
+    done
+}
+
+list() {
+    for l in $(cat .id_list); do
+        find $l/vars -type f -name "*.png" -exec convert "{}" -resize 250x250 "{}" \;
+    done
+}
+
+case $s in
+    list) list;;
+    *) range;;
+esac
